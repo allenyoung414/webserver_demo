02152635ae54dd8bf68a17984ee9eb0bea0427a9
allenyoung414 <you@example.com>
2023-07-09 16:57:25 +0800

    2023/7/9

--- CGImysql/sql_connection_pool.h
+++ CGImysql/sql_connection_pool.h
@@ -1,13 +1,67 @@
 #ifndef _CONNECTION_POOL_
 #define _CONNECTION_POOL_
 
-#include<stdio.h>
-#include<list>
-#include<mysql/mysql.h>
-#include<error.h>
-#include<string.h>
-#include<iostream>
-#include<string>
+#include <stdio.h>
+#include <list>
+#include <mysql/mysql.h>
+#include <error.h>
+#include <string.h>
+#include <iostream>
+#include <string>
 #include "../lock/locker.h"
 #include "../log/log.h"
+
+using namespace std;
+
+class connection_pool
+{
+public:
+	MYSQL *GetConnection();				 //获取数据库连接
+	bool ReleaseConnection(MYSQL *conn); //释放连接
+	int GetFreeConn();					 //获取连接
+	void DestroyPool();					 //销毁所有连接
+
+	//单例模式,获取连接池对象的唯一实例。
+	static connection_pool *GetInstance();
+
+	void init(string url, string User, string PassWord, string DataBaseName, int Port, int MaxConn, int close_log); 
+
+private:
+	connection_pool();
+	~connection_pool();
+
+	int m_MaxConn;  //最大连接数
+	int m_CurConn;  //当前已使用的连接数
+	int m_FreeConn; //当前空闲的连接数
+	locker lock;
+	list<MYSQL *> connList; //连接池
+	sem reserve;
+
+public:
+	string m_url;			 //主机地址
+	string m_Port;		 //数据库端口号
+	string m_User;		 //登陆数据库用户名
+	string m_PassWord;	 //登陆数据库密码
+	string m_DatabaseName; //使用数据库名
+	int m_close_log;	//日志开关
+};
+
+
+/*
+实现了一个连接对象的 RAII（Resource Acquisition Is Initialization）封装类
+用于在创建和销毁对象的过程中自动获取和释放数据库连接对象，以便确保资源的正确分配和释放，避免资源泄漏。
+*/
+class connectionRAII{
+
+public:
+	connectionRAII(MYSQL **con, connection_pool *connPool);
+	~connectionRAII();
+	
+private:
+	MYSQL *conRAII;
+	connection_pool *poolRAII;
+};
+/*
+将获取连接和释放连接封装在一个对象中
+避免在使用连接时忘记手动获取或释放连接，从而提高了代码的可靠性和安全性。*/
 #endif
--- webserver.h
+++ webserver.h
@@ -4,49 +4,49 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<errno.h>
-#include<fcntl.h>
-#include<stdlib.h>
-#include<cassert>
-#include<sys/epoll.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <cassert>
+#include <sys/epoll.h>
 
 #include "./threadpoll/threadpoll.h"
 #include "./http/http_conn.h"
 
-const int MAX_FD = 65536;
-const int MAX_EVENT_NUMBER = 10000;
-const int TIMESLOT = 5;
+const int MAX_FD = 65536;           //最大文件描述符
+const int MAX_EVENT_NUMBER = 10000; //最大事件数
+const int TIMESLOT = 5;             //最小超时单位
 
-class webserver
+class WebServer
 {
-private:
-    /* data */
 public:
-    webserver(/* args */);
-    ~webserver();
+    WebServer();
+    ~WebServer();
 
-    void init(int port, string user, string passWord, string databaseName,
-              int log_write, int opt_linger, int trigmode, int sql_num,
+    void init(int port , string user, string passWord, string databaseName,
+              int log_write , int opt_linger, int trigmode, int sql_num,
               int thread_num, int close_log, int actor_model);
-    void thread_poll();
-    void sql_poll();
-    void log_write();
-    void trig_mode();
-    void eventListen();
-    void eventLoop();
-    void timer(int connfd, struct sockaddr_in client_address);
-    void adjust_timer(util_timer *timer);
-    void deal_timer(util_timer *timer, int sockfd);
-    bool dealclinetdata();
-    bool dealwithsignal(bool& timeout, bool& stop_server);
-    void dealwithread(int sockfd);
-    void dealwithwrite(int sockfd);
+
+    void thread_pool();//创建线程池
+    void sql_pool();//创建数据库连接池
+    void log_write();//写日志
+    void trig_mode();//设置 IO 多路复用模式
+    void eventListen();//创建监听 socket 并将其加入 epoll 监听队列中
+    void eventLoop();//进入事件循环，不断调用 epoll_wait() 函数等待事件的发生，并调用相应的处理函数进行处理
+    void timer(int connfd, struct sockaddr_in client_address);//添加定时器
+    void adjust_timer(util_timer *timer);//调整定时器
+    void deal_timer(util_timer *timer, int sockfd);//处理定时器到期的事件
+    bool dealclinetdata();//处理客户端发来的请求
+    bool dealwithsignal(bool& timeout, bool& stop_server);//处理信号
+    void dealwithread(int sockfd);//处理可读事件
+    void dealwithwrite(int sockfd);//处理可写事件
 
 public:
+    //基础
     int m_port;
-    char* m_root;
+    char *m_root;
     int m_log_write;
     int m_close_log;
     int m_actormodel;
@@ -55,16 +55,28 @@ public:
     int m_epollfd;
     http_conn *users;
 
-    
+    //数据库相关
+    connection_pool *m_connPool;
+    string m_user;         //登陆数据库用户名
+    string m_passWord;     //登陆数据库密码
+    string m_databaseName; //使用数据库名
+    int m_sql_num;
 
+    //线程池相关
+    threadpool<http_conn> *m_pool;
+    int m_thread_num;
 
+    //epoll_event相关
+    epoll_event events[MAX_EVENT_NUMBER];
+
+    int m_listenfd;
+    int m_OPT_LINGER;
+    int m_TRIGMode;
+    int m_LISTENTrigmode;
+    int m_CONNTrigmode;
+
+    //定时器相关
+    client_data *users_timer;
+    Utils utils;
 };
-
-webserver::webserver(/* args */)
-{
-}
-
-webserver::~webserver()
-{
-}
 #endif
